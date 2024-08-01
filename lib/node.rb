c2c02e15729f4b2728a978960bd8b3f6b0b48f87
Roberto Nares <roberto.nares@gmail.com>
2024-08-01 09:24:23 -0700

    Implement node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
+# Class for a node consisting of a value and link to another node
 class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value: nil, next_node: nil)
+    @value = value
+    @next_node = next_node
+  end
 end
