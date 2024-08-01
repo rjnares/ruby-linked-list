bdeae3fe06eb6f8e05f814d3859507596eab7d23
Roberto Nares <roberto.nares@gmail.com>
2024-08-01 09:08:53 -0700

    Initialize project structure

---
+++ lib/linked_list.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Class for a linked list of nodes
+class LinkedList
+  def initialize
+    puts 'Initialized a new linked list!'
+  end
+end
---
+++ main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'lib/linked_list'
+
+list = LinkedList.new
+p list
